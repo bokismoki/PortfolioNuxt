250131eb971e849e79096a2d56afd4b6ce8d31f4
bokismoki <borisbosnjak084@gmail.com>
2020-01-12 21:28:37 +0100

    Changed <h1> content on Hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -9,9 +9,10 @@
         Welcome, I'm
         <span class="text-main-pink">Boris Bošnjak</span>.
       </h1>
-      <h1
-        class="text-lg font-semibold sm:text-xl lg:text-2xl xl:text-3xl"
-      >I'm a front end web developer.</h1>
+      <h1 class="text-lg font-semibold sm:text-xl lg:text-2xl xl:text-3xl">
+        I'm a
+        <span class="text-main-pink">WEB DEVELOPER</span>.
+      </h1>
       <a
         class="inline-block border-white border-2 mt-5 py-2 px-5 hover:border-main-blue hover:text-main-blue focus:border-main-blue focus:text-main-blue transition-colors transition-500 lg:py-3 lg:px-6 lg:text-lg"
         href="#projects"
