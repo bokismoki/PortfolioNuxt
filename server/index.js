e549663530ab09452a8255950e16755b78215a54
bokismoki <borisbosnjak084@gmail.com>
2020-03-04 20:20:51 +0100

    CORS

--- server/index.js
+++ server/index.js
@@ -4,6 +4,21 @@ const { Nuxt, Builder } = require('nuxt')
 const bodyParser = require('body-parser')
 const app = express()
 
+const allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain)
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 
